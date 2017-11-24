0d0ca66b059b90b3a0284e6f7945b9f70d07ffb1
Dodge W. Coates <dcoates91@gmail.com>
2017-11-24 12:00:11 -0500

    sync

--- js/demo.js
+++ js/demo.js
@@ -3,64 +3,298 @@
     "use strict";
     
     var main = document.getElementById("main");
-    var board1 = document.createElement("div");
-    var board2 = document.createElement("div");
-    
+    var board_el = document.createElement("div");
+    var json_output = document.getElementById("dest");
+    var output;
+    var eval_depth = 3;
+    var evaler;
+
     var boardSize = 300;
     
-    var fen1 = "rnbqk1nr/ppp2ppp/4p3/3p4/1b1PP3/2N5/PPP2PPP/R1BQKBNR w KQkq - 2 4";
-    var fen2 = "q4k2/8/8/3R3P/5N2/6K1/8/8 w - - 0 1"
-    
-    function createBoard1(el)
+    var fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -";
+
+    function createBoard(el, fen)
     {
         var board = BOARD(el);
         board.size_board(boardSize, boardSize);
-        board.set_board(fen1);
-        board.wait(); /// Disable movement
-        
-        /// board.highlight_square(rank, file, color)
-        ///NOTE: Both `rank` and `file` are numbers starting from a1 (0, 0).
-        ///NOTE: `color` can be any of the following: "blue", "red", "green", "yellow", "teal", "orange", "purple", "pink"
-        ///      These colors are defined in `board.highlight_colors` and the CSS is in board.css.
-        board.highlight_square(2, 2, "red");
-        board.highlight_square(3, 1, "green");
-        
-        /// board.arrow_manager.draw(startRank, startFile, endRank, endFile, CSSColor)
-        ///NOTE: All ranks and files are numbers starting from a1 (0, 0).
-        ///NOTE: `CSSColor` is any valid CSS color value, eg., "blue" or "rgba(0, 0, 240, .6)".
-        ///      `board.color_values` has predefined CSS color values in the following order: "blue", "red", "green", "yellow", "teal", "orange", "purple", "pink"
-        board.arrow_manager.draw(3, 1, 0, 4, board.color_values[board.highlight_colors.indexOf("red")]);
+        board.set_board(fen);
+        board.wait();
     }
-    
-    function createBoard2(el)
+
+    function formatOutput()
     {
-        var board = BOARD(el);
-        board.size_board(boardSize, boardSize);
-        board.set_board(fen2);
-        board.wait(); /// Disable movement
-        
-        /// board.highlight_square(rank, file, color)
-        ///NOTE: Both `rank` and `file` are numbers starting from a1 (0, 0).
-        ///NOTE: `color` can be any of the following: "blue", "red", "green", "yellow", "teal", "orange", "purple", "pink"
-        ///      These colors are defined in `board.highlight_colors` and the CSS is in board.css.
-        board.highlight_square(7, 3, "green");
-        
-        /// board.arrow_manager.draw(startRank, startFile, endRank, endFile, CSSColor)
-        ///NOTE: All ranks and files are numbers starting from a1 (0, 0).
-        ///NOTE: `CSSColor` is any valid CSS color value, eg., "blue" or "rgba(0, 0, 240, .6)".
-        ///      `board.color_values` has predefined CSS color values in the following order: "blue", "red", "green", "yellow", "teal", "orange", "purple", "pink"
-        board.arrow_manager.draw(4, 3, 7, 3, board.color_values[board.highlight_colors.indexOf("green")]);
+        // long bit of test json
+        return JSON.parse('{\"Board\":{\"PIECES\":{\"27\":{\"xray2 (d->e)\":9223372036854775808,\"pawns on same color\":85899345920,\"valid fork\":17180917760,\"reachable outposts\":85899345920,\"attacked king squares\":0,\"attacked squares\":18049668782227969,\"partially attacked squares\":18049668782227969}}},\"Scores\":{\"PIECES\":{\"49\":{\"close enemies\":0,\"king safety\":0},\"27\":{\"interferring pawns\":0,\"reachable outpost\":0,\"mobility\":58}},\"TRACE\":{\"Imbalance\":{\"imbalance\":-25},\"Imbalance\":{\"imbalance\":-25},\"Mobility\":{\"mobility\":58},\"Threat\":{\"threat by pawn push\":0,\"hanging threats\":54,\"threat by weak minor\":33},\"Passed\":null,\"Space\":null,\"Total\":null}}}');;
     }
-    
+
+    function update()
+    {
+        updateFen();
+        updateBoard();
+        updateJsonOutput();
+    }
+
+    function updateEngine()
+    {
+        function eval_pos()
+        {
+            // If we are in the middle of an eval, stop it and do the latest one.
+            if (evaler.busy) {
+                // TEMP
+                evaler.stop = true;
+                return evaler.send("stop");
+            }
+
+            evaler.stop = false;
+            evaler.busy = true;
+
+            // simplified
+            evaler.send("go depth " + eval_depth);
+        }
+
+        evaler = load_engine();
+
+        evaler.send("uci", function onuci(str)
+                    {
+                        evaler.send("isready", function onready()
+                                    {
+                                        if (board.get_mode() === "wait") {
+                                            alert("wait") // test
+                                        }
+                                    });
+                    });
+
+        evaler.send("position " + fen);
+
+        eval_pos();
+    }
+
+    function updateFen()
+    {
+        fen = document.getElementById('inputFen').value;
+    }
+
+    function updateBoard()
+    {
+        createBoard(board_el, fen);
+
+        main.appendChild(board_el);
+    }
+    function updateJsonOutput()
+    {
+        output = formatOutput();
+
+        json_output.replaceChild(json_output.firstChild, renderjson(output));
+    }
+
+    function load_engine()
+    {
+        var worker = new Worker("js/stockfish6.js"),
+            engine = {started: Date.now()},
+            que = [];
+
+        function get_first_word(line)
+        {
+            var space_index = line.indexOf(" ");
+
+            /// If there are no spaces, send the whole line.
+            if (space_index === -1) {
+                return line;
+            }
+            return line.substr(0, space_index);
+        }
+
+        function determine_que_num(line, que)
+        {
+            var cmd_type,
+                first_word = get_first_word(line),
+                cmd_first_word,
+                i,
+                len;
+
+            if (first_word === "uciok" || first_word === "option") {
+                cmd_type = "uci"
+            } else if (first_word === "readyok") {
+                cmd_type = "isready";
+            } else if (first_word === "bestmove" || first_word === "info") {
+                cmd_type = "go";
+            } else {
+                /// eval and d are more difficult.
+                cmd_type = "other";
+            }
+
+            len = que.length;
+
+            for (i = 0; i < len; i += 1) {
+                cmd_first_word = get_first_word(que[i].cmd);
+                if (cmd_first_word === cmd_type || (cmd_type === "other" && (cmd_first_word === "d" || cmd_first_word === "eval"))) {
+                    return i;
+                }
+            }
+
+            /// Not sure; just go with the first one.
+            return 0;
+        }
+
+        worker.onmessage = function (e)
+        {
+            var line = e.data,
+                done,
+                que_num = 0,
+                my_que;
+
+            /// Stream everything to this, even invalid lines.
+            if (engine.stream) {
+                engine.stream(line);
+            }
+
+            /// Ignore invalid setoption commands since valid ones do not repond.
+            if (line.substr(0, 14) === "No such option") {
+                return;
+            }
+
+            que_num = determine_que_num(line, que);
+
+            my_que = que[que_num];
+
+            if (!my_que) {
+                return;
+            }
+
+            if (my_que.stream) {
+                my_que.stream(line);
+            }
+
+            if (typeof my_que.message === "undefined") {
+                my_que.message = "";
+            } else if (my_que.message !== "") {
+                my_que.message += "\n";
+            }
+
+            my_que.message += line;
+
+            /// Try to determine if the stream is done.
+            if (line === "uciok") {
+                /// uci
+                done = true;
+                engine.loaded = true;
+            } else if (line === "readyok") {
+                /// isready
+                done = true;
+                engine.ready = true;
+            } else if (line.substr(0, 8) === "bestmove") {
+                /// go [...]
+                done = true;
+                /// All "go" needs is the last line (use stream to get more)
+                my_que.message = line;
+            } else if (my_que.cmd === "d" && line.substr(0, 15) === "Legal uci moves") {
+                done = true;
+            } else if (my_que.cmd === "eval" && /Total Evaluation[\s\S]+\n$/.test(my_que.message)) {
+                done = true;
+            } else if (line.substr(0, 15) === "Unknown command") {
+                done = true;
+            }
+            ///NOTE: Stockfish.js does not support the "debug" or "register" commands.
+            ///TODO: Add support for "perft", "bench", and "key" commands.
+            ///TODO: Get welcome message so that it does not get caught with other messages.
+            ///TODO: Prevent (or handle) multiple messages from different commands
+            ///      E.g., "go depth 20" followed later by "uci"
+
+            if (done) {
+                if (my_que.cb && !my_que.discard) {
+                    my_que.cb(my_que.message);
+                }
+
+                /// Remove this from the que.
+                G.array_remove(que, que_num);
+            }
+        };
+
+        engine.send = function send(cmd, cb, stream)
+        {
+            cmd = String(cmd).trim();
+
+            ///TODO: Destroy the engine.
+            if (cmd === "quit") {
+                return;
+            }
+
+            /// Only add a que for commands that always print.
+            ///NOTE: setoption may or may not print a statement.
+            if (cmd !== "ucinewgame" && cmd !== "flip" && cmd !== "stop" && cmd !== "ponderhit" && cmd.substr(0, 8) !== "position"  && cmd.substr(0, 9) !== "setoption") {
+                que[que.length] = {
+                    cmd: cmd,
+                    cb: cb,
+                    stream: stream
+                };
+            }
+
+            worker.postMessage(cmd);
+        };
+
+        engine.stop_moves = function stop_moves()
+        {
+            var i,
+                len = que.length;
+
+            for (i = 0; i < len; i += 1) {
+                if (debugging) {
+                    console.log(i, get_first_word(que[i].cmd))
+                }
+                /// We found a move that has not been stopped yet.
+                if (get_first_word(que[i].cmd) === "go" && !que[i].discard) {
+                    engine.send("stop");
+                    que[i].discard = true;
+                }
+            }
+        }
+
+        engine.get_cue_len = function get_cue_len()
+        {
+            return que.length;
+        }
+
+        return engine;
+    }
+
+    function bitboardToArray(bb)
+    {
+        var bb2 = bb.toString(2),
+            arr = [];
+        alert(bb2);
+        for (var i = 0; i < bb2.length; i++) {
+            if (bb2[i] === "1") {
+                var x = i % 8;
+                var y = Math.ceil(i / 8);
+                arr.push([x, y]);
+            }
+        }
+
+        return arr;
+    }
+
+    function highlightBoard(locs) {
+        for (var loc in locs) {
+            // board.highlight_square(loc[0], loc[1], "red");
+            // alert(loc);
+        }
+    }
+
     function init()
     {
-        createBoard1(board1);
-        createBoard2(board2);
-        
-        main.appendChild(board1);
-        main.appendChild(document.createElement("br"));
-        main.appendChild(board2);
+        updateBoard();
+        updateEngine(); // temp
+        // evaler = load_engine();
+
+        output = formatOutput();
+
+        var array = bitboardToArray(4294966271);
+        highlightBoard(array);
+
+        //renderjson.set_property_list(["a", "c", "b", "d", "g", "h"]);
+        json_output.appendChild(renderjson(output));
     }
-    
+
+    document.getElementById('buttonFen').addEventListener("click", update)
     document.addEventListener("DOMContentLoaded", init);
 }());
