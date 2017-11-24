24f8857358ae439df53c7de9f1403046abf9c429
Dodge W. Coates <dodge.w.coates@gmail.com>
2017-11-24 14:12:26 -0500

    add json

--- js/demo.js
+++ js/demo.js
@@ -4,6 +4,8 @@
     
     var main = document.getElementById("main");
     var board_el = document.createElement("div");
+    var json_output = document.getElementById("dest");
+    var output;
 
     var boardSize = 300;
 
@@ -24,9 +26,26 @@
         main.appendChild(board_el);
     }
 
+    function formatOutput()
+    {
+        // long bit of test json
+        return JSON.parse('{\"Board\":{\"PIECES\":{\"27\":{\"xray2 (d->e)\":9223372036854775808,\"pawns on same color\":85899345920,\"valid fork\":17180917760,\"reachable outposts\":85899345920,\"attacked king squares\":0,\"attacked squares\":18049668782227969,\"partially attacked squares\":18049668782227969}}},\"Scores\":{\"PIECES\":{\"49\":{\"close enemies\":0,\"king safety\":0},\"27\":{\"interferring pawns\":0,\"reachable outpost\":0,\"mobility\":58}},\"TRACE\":{\"Imbalance\":{\"imbalance\":-25},\"Imbalance\":{\"imbalance\":-25},\"Mobility\":{\"mobility\":58},\"Threat\":{\"threat by pawn push\":0,\"hanging threats\":54,\"threat by weak minor\":33},\"Passed\":null,\"Space\":null,\"Total\":null}}}');;
+    }
+
+    function updateJsonOutput()
+    {
+        output = formatOutput();
+
+        json_output.replaceChild(json_output.firstChild, renderjson(output));
+    }
+
     function init()
     {
         updateBoard();
+
+        output = formatOutput();
+
+        json_output.appendChild(renderjson(output));
     }
     
     document.addEventListener("DOMContentLoaded", init);
