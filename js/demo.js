a6301d1cc7fde4b660cf41565e0f2a72c9493673
Dodge W. Coates <dodge.w.coates@gmail.com>
2017-11-27 00:48:44 -0500

    remove old alert

--- js/demo.js
+++ js/demo.js
@@ -112,22 +112,20 @@
         }
 
         renderjson.set_replacer(function(k,v) {
-            alert('hello');
             var obj_from_dom = function (el) {
-                if (typeof(el) == "number") return "FUCK"; // Capture NaNs and Infinity
                 if (el.nodeType == el.TEXT_NODE)
                     return el.data;
                 var attributes="";
                 if (el.attributes)
                     for (var i=0; i<el.attributes.length; i++)
-                        attributes += " "+el.attributes.item(i).name + "=\"" + el.attributes.item(i).value + "\"";
+        s                attributes += " "+el.attributes.item(i).name + "=\"" + el.attributes.item(i).value + "\"";
                 var obj = {};
                 obj["<"+el.tagName+attributes+">"] = Array.prototype.map.call(el.childNodes, obj_from_dom);
                 return obj;
             };
             if (v === window) return "<window>";
             if (v === document) return "<document>";
-            if (typeof(v) == "number") return "FUCK"; // Capture NaNs and Infinity
+            if (typeof(v) == "number") return v; // Capture NaNs and Infinity
             if (typeof(v) == "object" && v && "nodeType" in v) return obj_from_dom(v);
             else return v;
         });
